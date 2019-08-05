f912fff6cb29206bfd232bcdc9f6397f9cfa5fd3
ashleych <ashley.cherian@gmail.com>
2019-08-05 12:29:36 +0530

    selectedModelDiagnostics Added

--- test.R
+++ test.R
@@ -4,6 +4,15 @@
 
 
 vars<-c("ECI_yoy_ch_3QMA_lag_4", "avg_oil_pri_barrel_3QMA", "Rl_est_Dub_q_yoy_ch_lag_1", "avg_oil_pri_barrel_3QMA_lag_2", "Non_oil_ECI_yoy_ch_3QMA_lag_3", "Non_oil_ECI_yoy_ch_6QMA", "avg_oil_pri_barrel", "avg_oil_pri_barrel_3QMA_lag_1", "avg_oil_pri_barrel_6QMA_lag_1", "avg_oil_pri_barrel_lag_2", "avg_oil_pri_barrel_lag_3")
-allModels<-modelDeveloper("DR",vars,2)
-allModelsDiagnostics<-modelDiagnostics(allModels)
-modelEvaluator(allModelsDiagnostics)
+allModels<-modelDeveloper("DR",vars, trainData = train_df,no_of_vars = 2)
+allModelsDiagnostics<-modelDiagnostics(allModels,)
+allModelEvaluated<-modelEvaluator(allModelsDiagnostics)
+table(allModelEvaluated$FinalResults)
+allModelEvaluated[FinalResults=="PASS",]
+
+
+selectedModel <-
+  "DR ~ ECI_yoy_ch_3QMA_lag_4+avg_oil_pri_barrel_3QMA_lag_1" # have chosen a model that passes all tests
+
+selectedModelObject <- allModels[[selectedModel]]
+selectedModelDiagnostics(selectedModel, allModelEvaluated)
