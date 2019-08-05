8254ce301bccf053c4fa59a6373dc72fe42f71ea
ashleych <ashley.cherian@gmail.com>
2019-08-05 16:20:55 +0530

    Changed R version dependency to 3.0

--- test.R
+++ test.R
@@ -1,11 +1,11 @@
 # mdata<-readxl::read_excel("data/MacroDataAC.xlsx")
-# validationSampler(mdata,1:29,30:33,1:48)
+validationSampler(mdata,1:29,30:33,1:48)
 
 
 
 vars<-c("ECI_yoy_ch_3QMA_lag_4", "avg_oil_pri_barrel_3QMA", "Rl_est_Dub_q_yoy_ch_lag_1", "avg_oil_pri_barrel_3QMA_lag_2", "Non_oil_ECI_yoy_ch_3QMA_lag_3", "Non_oil_ECI_yoy_ch_6QMA", "avg_oil_pri_barrel", "avg_oil_pri_barrel_3QMA_lag_1", "avg_oil_pri_barrel_6QMA_lag_1", "avg_oil_pri_barrel_lag_2", "avg_oil_pri_barrel_lag_3")
 allModels<-modelDeveloper("DR",vars, trainData = train_df,no_of_vars = 2)
-allModelsDiagnostics<-modelDiagnostics(allModels,)
+allModelsDiagnostics<-modelDiagnostics(allModels)
 allModelEvaluated<-modelEvaluator(allModelsDiagnostics)
 table(allModelEvaluated$FinalResults)
 allModelEvaluated[FinalResults=="PASS",]
@@ -21,3 +21,5 @@ predicted_df
 
 
 selectedModelCharter(selectedModel,allModelEvaluated)
+
+selectedMOdelDf<-allModelsDiagnostics[model==selectedModel]
