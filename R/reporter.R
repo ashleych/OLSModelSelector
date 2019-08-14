21bb50fa0f94858cf1c6ac7deaf80e055b466a1d
ashleych <ashley.cherian@gmail.com>
2019-08-14 12:43:43 +0530

    fixed Reporter.R issue with gitignore

---
+++ R/reporter.R
@@ -0,0 +1,35 @@
+#' Knit the rmarkdown into an HTML document
+#'
+#' @param modelList model list in LHS~RHS form
+#' @param output_file name of the output file
+#' @param output_dir Name of the output directory
+#' @param report_title Name of the report
+#' @return
+#' @export
+#' @examples
+#' models<-c("DR~avg_oil_pri_barrel_lag_3+avg_oil_pri_barrel_lag_2","DR~avg_oil_pri_barrel_lag_2","DR~Non_oil_ECI_yoy_ch_6QMA+avg_oil_pri_barrel_6QMA_lag_1")
+#' call_rmd(models,report_title = "Retail")
+
+call_rmd <- function(model_list, output_file = "report.html", output_dir = getwd(), report_title = "Corporate", ...) {
+
+
+  ## Get directory of report markdown template
+  report_dir <- system.file("rmdTemplate/selectedModelReport.rmd", package = "ST.auto.1")
+
+
+  render(
+    report_dir,
+    output_file = output_file,
+    output_dir = output_dir,
+    intermediates_dir = output_dir,
+    params = list(model_list = model_list,
+                  set_title = report_title)
+  )
+
+
+  ## Open report
+  report_path <- path.expand(file.path(output_dir, output_file))
+  browseURL(report_path)
+
+}
+
