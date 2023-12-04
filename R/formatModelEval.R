bb9a3adc174b873aad5e8b03035610979f6ec30d
ashleych <ashley.cherian@gmail.com>
2023-12-04 14:50:39 +0530

    optimised formatModelEval

--- R/formatModelEval.R
+++ R/formatModelEval.R
@@ -7,33 +7,13 @@
 #' formatModelEvalResults(allModelEvaluated)
 
 formatModelEvalResults <- function(allModelEvaluated) {
-  renameAsOrderedVariables <- function (row) {
-    #function to rename the mec
-    renamedVar <- NA
-    model <- row$model
-    model_LHS <- trimws(unlist(strsplit(model, "[~]"))[[1]])
-    RHS_combined <- trimws(unlist(strsplit(model, "[~]"))[[2]])
-    model_RHS <- trimws(unlist(strsplit(RHS_combined, '[+]')))
-    for (index in 1:length(model_RHS)) {
-      pattern <- model_RHS[index]
-      text <- row$variable
-      replacement <- paste0("Variable", index)
-      if (grepl(paste0(pattern, "$"), text))
-      {
-        row$variableRenamed <- gsub(pattern, replacement, text)
-        renamedVar <- gsub(pattern, replacement, text)
-      }
-      if (!is.na(renamedVar)) {
-        break
-      }
-    }
-    
-    return(renamedVar)
-    
-  }
+  evaluatedModels <- copy(allModelEvaluated)
+  
+  
   orderColumns <- function(DT) {
     firstSet <- c('model', 'Intercept')
-    secondSetPattern <- "^Var" # estimates
+    secondSetPattern <- "^Var" 
+    estimatesPattern<-"^Estimate"
     thirdSetPattern <- "^se.Var"
     fourthSetPattern <- "^t.Var"
     fifthSetPattern <- "^p.Var"
@@ -88,7 +68,9 @@ formatModelEvalResults <- function(allModelEvaluated) {
     # Arrange columns based on sets and patterns
     ordered_cols <- c(
       firstSet,
+
       sortColumns(secondSetPattern),
+      sortColumns(estimatesPattern),
       sortColumns(thirdSetPattern),
       sortColumns(fourthSetPattern),
       sortColumns(fifthSetPattern),
@@ -100,29 +82,123 @@ formatModelEvalResults <- function(allModelEvaluated) {
     DT <- DT[, ..ordered_cols]
     return(DT)
   }
-  colnames(allModelEvaluated) <-
-    gsub("\\(|\\)", "", colnames(allModelEvaluated)) # to remove paranthesis from intercept column names
-  ids = c( "model", 'Intercept', "VifMax", "se.Intercept", "t.Intercept", "p.Intercept", "R2", "adjR2", "MAPE_test", "MAPE_training", "RMSE", "pF", "BP_statistic", "BP_pvalue", "dw_statistic", "dw_pvalue", "shapiro_statistic", "shapiro_pvalue", "adf_statistic", "adf_pvalue", "KPSS_statistic", "KPSS_pvalue", "dfdirectioncheck", "bg_statistic", "bg_pvalue", "Heteroskedasticity", "BP_results", "Normality", "shapiro_results", "Autocorrelation_bg", "bg_results", "Autocorrelation", "dw_results", "Stationarity", "KPSS_results", "adf_results", "significant", "FinalResults" ) #melt the table
-  #
+  # split into response variable and independent variables
+  evaluatedModels[, `:=`(c("ResponseVar", "IndepVar"),
+                         tstrsplit(model, "[~]", fill = NA))]
+  
+  # split into independente variables into other columns and save it in a different DT for instance mev1 + Mev2 wil be split into two columns
+  # the + sign is removed
+  
+  splitTemp <- evaluatedModels[,  tstrsplit(IndepVar, "[+]", fill = NA)]
+  
+  colnames(splitTemp) <-
+    paste0("Variable", 1:length(names(splitTemp))) #rename these are variable1,2 etc
+  splitTemp[, names(splitTemp) := lapply(.SD, trimws), .SDcols = names(splitTemp)] # remove whitespaces if any
+  variablesNamesSequence <- colnames(splitTemp)
+  
+  evaluatedModels <- cbind(evaluatedModels, splitTemp)
+  evaluatedModels$IndepVar <- NULL
+  
+  colnames(evaluatedModels) <-
+    gsub("\\(|\\)", "", colnames(evaluatedModels)) # to remove paranthesis from intercept column names
+  ids = c(
+    "model",
+    "ResponseVar",
+    'Intercept',
+    "VifMax",
+    "VIF_error",
+    "se.Intercept",
+    "t.Intercept",
+    "p.Intercept",
+    "R2",
+    "adjR2",
+    "MAPE_test",
+    "MAPE_training",
+    "RMSE",
+    "pF",
+    "BP_statistic",
+    "BP_pvalue",
+    "dw_statistic",
+    "dw_pvalue",
+    "shapiro_statistic",
+    "shapiro_pvalue",
+    "adf_statistic",
+    "adf_pvalue",
+    "KPSS_statistic",
+    "KPSS_pvalue",
+    "dfdirectioncheck",
+    "bg_statistic",
+    "bg_pvalue",
+    "Heteroskedasticity",
+    "BP_results",
+    "Normality",
+    "shapiro_results",
+    "Autocorrelation_bg",
+    "bg_results",
+    "Autocorrelation",
+    "dw_results",
+    "Stationarity",
+    "KPSS_results",
+    "adf_results",
+    "significant",
+    "FinalResults"
+  ) 
+  
+  
+  #melt the table
+  ids <- c(ids, variablesNamesSequence)
+  # these cols will not be 'melted'
+  
+  
   meltedEval <-
-    data.table::melt(allModelEvaluated,
+    data.table::melt(evaluatedModels,
                      id = ids,
                      variable.factor = FALSE)
-  meltedEval <- meltedEval[!is.na(value)] #remvoe NAs
+  meltedEval <- meltedEval[!is.na(value)] #remove NAs
   
   meltedEval$variableRenamed <- NA
-  for (row in 1:nrow(meltedEval)) {
-    meltedEval$variableRenamed[row] <-
-      renameAsOrderedVariables(meltedEval[row]) #rename variables as Var1, var 2 etc
+  
+  
+  
+  splitVariablesDT <-
+    meltedEval[,  tstrsplit(
+      variable,
+      "[.]",
+      fill = NA,
+      names = c('estimateType', 'variableSuffix')
+    )] # split the variable names to remove the prefixes like t.Mev1, p.mev2,p.vif etc
+  splitVariablesDT[, names(splitVariablesDT) := lapply(.SD, trimws), .SDcols = names(splitVariablesDT)]
+  
+  noEstimatesRows <- is.na(splitVariablesDT$variableSuffix)
+  splitVariablesDT[noEstimatesRows, variableSuffix := estimateType] # copy teh variable suffix from EstimateType
+  splitVariablesDT[noEstimatesRows, estimateType := NA] # make the estimate type for these NA
+  meltedEval <- cbind(meltedEval, splitVariablesDT)
+  meltedEval$NewVariableSuffix <- ""
+  
+  for (v in variablesNamesSequence) {
+    meltedEval[variableSuffix == get(v), NewVariableSuffix := v]
   }
+  
+  #   variable  ,variableSuffix,variableRenamed
+  meltedEval$variable <- NULL
+  
+  
+  # merge NEwVariableSuffix and estimate tyoe
+  meltedEval[, variable := ifelse(
+    !is.na(estimateType),
+    paste0(estimateType, ".", NewVariableSuffix),
+    paste0("Estimate", ".", NewVariableSuffix)
+  )]
+  meltedEval$estimateType <- NULL
+  meltedEval$NewVariableSuffix <- NULL
   
   
   formula <-
-    paste0(paste(ids, collapse = "+"), " ~ ", "variableRenamed")
+    paste0(paste(ids, collapse = "+"), " ~ ", "variable")
   wideModelEval <-
     dcast(meltedEval, formula = formula , value.var = "value") #make into wide format
   
   
   condensedModelEval <- orderColumns(wideModelEval)
-  return (condensedModelEval)
+  return(condensedModelEval)
 }
